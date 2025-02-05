86a62e55964f43fce6319d6ed974728f108fe641
PhichaPrim <152593599+PhichaPrim@users.noreply.github.com>
2025-02-05 09:14:44 +0700

    Fin

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        Character Max = new Character("Max");
+        Max.swingSword();
+        Max.swingSword();
+        Max.swingSword();
+        Max.swingSword();
+        Max.swingSword();
+        Max.takeDamage(30);
+        Max.takeDamage(20);
+        Max.takeDamage(50);
+        Max.rest();
+        Max.showDetails();
+    }
+}
